ddc86238e08332b215d58266ce9b3e42fee1a649
shido <banas49@mail.ru>
2016-06-02 19:52:36 +0500

    Работает

--- homework5(2)/src/UTFIsCool.java
+++ homework5(2)/src/UTFIsCool.java
@@ -1,19 +1,43 @@
-import java.io.EOFException;
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.util.Random;
 
 /**
  * Created by shido on 31.05.16.
  */
 public class UTFIsCool {
+
+    final static Random random = new Random();
+
+    public static String readUTF(String fileName, long offset) throws IOException {
+
+        FileInputStream is = new FileInputStream(fileName);
+        Reader fileReader = new InputStreamReader(is, "UTF-8");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        fileReader.skip(offset);
+
+        int charsRead;
+        char buf[] = new char[256];
+
+        //Read until there is no more characters to read.
+        while ((charsRead = fileReader.read(buf)) > 0) {
+            stringBuilder.append(buf, 0, charsRead);
+        }
+
+        fileReader.close();
+
+        return stringBuilder.toString();
+    }
     
-    public void main(String[] args){
+    public static void main(String[] args){
         try {
             RandomAccessFile raf = new RandomAccessFile(args[0], "r");
 
             try {
                 Boolean isUTFChar = false;
-                long pos = -1;
+                raf.seek(random.nextInt((int)raf.length()));
+                long pos = raf.getFilePointer();
+                System.out.print(String.format("Начало чтения с: %s\n", pos));
 
                 while (!isUTFChar){
                     pos = raf.getFilePointer();
@@ -26,18 +50,15 @@ public class UTFIsCool {
                     if ((bit7 == 1) && (bit6 == 1)) isUTFChar = true; //Начало UTF8 символа
 
                     if (!isUTFChar){
-                        System.out.print(String.format("Пропущен символ с бинарным кодом: %8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+                        System.out.print(String.format("Пропущен символ с бинарным кодом: %8s\n", Integer.toBinaryString(b & 0xFF)));
                     }
                 }
 
+                System.out.print(String.format("Первый символ в позиции: %s\n\n", pos));
                 raf.seek(pos);
 
-                //Вывод файла на экран как строки
-                String smb;
-                while (true) {
-                    smb = raf.readUTF();
-                    System.out.print(smb);
-                }
+                String str = readUTF(args[0], pos);
+                System.out.print(str);
             }
             catch(EOFException e){
                 System.out.println("\nЧтение закончено");
